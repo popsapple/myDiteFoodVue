8c91e7c42af47055b91eb21b8e7b08057f0d79e4
hyeongaram <saysun34@gmail.com>
2019-09-22 03:05:25 +0900

    정적파일 동작 추가

--- src/main/index.js
+++ src/main/index.js
@@ -1,67 +1,56 @@
-'use strict'
+"use strict";
+/* eslint-disable */
+const winURL =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`;
 
-import { app, BrowserWindow } from 'electron'
+const { app, BrowserWindow, protocol } = require("electron");
+const path = require("path");
+const url = require("url");
 
-/**
- * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
- */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname:
+        "index.html" /* Attention here: origin is path.join(__dirname, 'index.html') */,
+      protocol: "file",
+      slashes: true
+    })
+  );
+
+  mainWindow.on("closed", function() {
+    mainWindow = null;
+  });
 }
 
-let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+app.on("ready", () => {
+  protocol.interceptFileProtocol(
+    "file",
+    (request, callback) => {
+      const url = request.url.substr(7); /* all urls start with 'file://' */
+      callback({ path: path.normalize(`${__dirname}/${url}`) });
+    },
+    err => {
+      if (err) console.error("Failed to register protocol");
+    }
+  );
+  createWindow();
+});
 
-function createWindow () {
-  /**
-   * Initial window options
-   */
-  mainWindow = new BrowserWindow({
-    height: 563,
-    useContentSize: true,
-    width: 1000
-  })
-
-  mainWindow.loadURL(winURL)
-
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", function() {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", function() {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
-
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
-
-/*
-import { autoUpdater } from 'electron-updater'
-
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
-
-app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
- */
+});
+/* eslint-enable */
